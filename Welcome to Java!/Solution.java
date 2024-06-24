3d5df6bd4c03b4fcfa87ba8b1c4e2d80779bc8db
wolfie <yavorhristozov@gmail.com>
2024-06-24 20:21:34 +0300

    add :  Solution
    Welcome to Java!

---
+++ Welcome to Java!/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+  public static void main(String[] args) {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+  }
+}
+
+
+
